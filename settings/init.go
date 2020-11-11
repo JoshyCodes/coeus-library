7e6768cb76e99e2cacfaac82352c720abe019bf5
JFischer <jfischer@pop-os.localdomain>
2020-11-11 16:31:49 -0500

    removed db, added settings loader

---
+++ settings/init.go
@@ -0,0 +1,23 @@
+package settings
+
+import (
+	"log"
+	"github.com/joho/godotenv"
+)
+
+func Init(status bool) {
+	var configFile string
+
+	if status {
+		log.Println("Loading Production Settings...")
+		configFile = "configs/env/prod/.env"
+	} else {
+		log.Println("Loading Development Settings...")
+		configFile = "configs/env/dev/.env"
+	}
+	
+	err := godotenv.Load(configFile)
+	if err != nil {
+	  log.Println("Error loading .env file")
+	}
+}
